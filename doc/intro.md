ccc2fd8587fb1fb5bef89294bf912ff257080794
Logan Campbell <colin@logaan.net>
2015-07-18 07:14:26 +1000

    lein new isomorphic validations

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to isomorphic-validations
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/isomorphic_validations/core_test.clj
@@ -0,0 +1,7 @@
+(ns isomorphic-validations.core-test
+  (:require [clojure.test :refer :all]
+            [isomorphic-validations.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
