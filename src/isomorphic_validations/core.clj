85c34efa8906908428771408598448ecbc2e8903
Logan Campbell <colin@logaan.net>
2015-07-18 19:52:53 +1000

    basic post

--- src/isomorphic_validations/core.clj
+++ src/isomorphic_validations/core.clj
@@ -1,6 +1,40 @@
-(ns isomorphic-validations.core)
+(ns isomorphic-validations.core
+  (:require [compojure.core :refer :all]
+            [org.httpkit.server :refer [run-server]]
+            [hiccup.core :refer [html]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app
+  (GET "/" request
+       (html
+        [:h1 "Hello World"]
+        [:form {:action "/post" :method "POST"}
+         [:input {:name "fullname"}]
+         [:input {:type "submit"}]]))
+  (POST "/post" request
+        #break (:params request)
+        "Done"))
+
+(def site
+  (wrap-defaults app api-defaults))
+
+(defonce server
+  (atom nil))
+
+(defn start-server []
+  (reset! server (run-server #'site {:port 8080})))
+
+(defn stop-server []
+  (@server))
+
+(defn restart-server []
+  (stop-server)
+  (start-server))
+
+(comment
+
+  (start-server)
+
+  (restart-server)
+
+  )
