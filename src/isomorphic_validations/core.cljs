612f20ea22b8fa1aed35226f6b6cf33fa65ab463
Logan Campbell <colin@logaan.net>
2015-07-28 15:37:03 +1000

    showing js validation errors

--- src/isomorphic_validations/core.cljs
+++ src/isomorphic_validations/core.cljs
@@ -5,13 +5,24 @@
 
 (enable-console-print!)
 
+(defn append-error [field-name text]
+  (let [node (aget (js/document.getElementById field-name) "parentElement")
+        error (js/document.createElement "p")]
+    (.appendChild error (js/document.createTextNode text))
+    (aset (aget error "style") "color" "red")
+    (.appendChild node error)))
+
 (defn validate-form []
   (let [user {:fullname (aget (js/document.getElementById "fullname") "value")
               :email (aget (js/document.getElementById "email") "value")
               :username (aget (js/document.getElementById "username") "value")
               :password (aget (js/document.getElementById "password") "value")
               :confirm-password (aget (js/document.getElementById "confirm-password") "value")}]
-    (pprint (user-errors user))
-  false))
+    (doall
+     (for [[field errors] (user-errors user)
+           error errors]
+       (append-error (name (first field)) error)))
+    false))
 
 (aset (js/document.getElementById "signup") "onsubmit" validate-form)
+
